ef68777720060999076b192509cfe1d22ac67568
Lariana C01~ <cordero.lariana@gmail.com>
2022-10-28 08:47:29 -0400

    Updated JS

--- script.js
+++ script.js
@@ -21,11 +21,60 @@ storedBodyEl.style.backgroundColor = '#3CB371'
 
 /* Part 3
 Select DOM's Favorite Things list and remove the last list item.*/ 
-const faveThingsEl = document.querySelector("#favorite-things")
+// const sideBar = document.querySelector("side-bar")
+// const faveThings = document.getElementById('favorite-things')
 
-faveThingsEl.classList.remove(faveThingsEl.lastElementChild)
+// sideBar.removeChild(faveThings)
+// const sideBar = document.querySelector("side-bar")
+// const faveThingsLi = document.getElementById("#favorite-things");
+// // //faveThingsLi.removeChild(faveThingsLi.lastElementChild)
+// sideBar.removeChild[5](faveThingsLi);
+// //im not sure why this is not working
+
+
+// Part 4
+// Select all .special-title class elements and change their font-size to 2rem. Remember you might have to iterate through the list of elements
+
+let specialEl = document.getElementsByClassName("special-title");
+for (let i = 0; i < specialEl.length; i++) {
+    specialEl[i].style.fontSize = '2rem';
+}
+// console.log(specialEl)
 
 // Part 5
 // Turns out DOM never raced in Chicago. Access the Past Races list and remove Chicago.
 const pastRacesEl = document.querySelector('#past-races')
-pastRacesEl[3].remove
+pastRacesEl[3].removeChid()
+
+
+
+
+// Part 6
+// Let's add to DOM's Past Races list. Create a new <li> element, change the new <li> text to the name of a city, and append it to the Past Races list.
+
+const newLi = document.createElement("li");
+newLi.innerText = "New York City";
+// document.newLi.appendchild --not right
+const pastRaces = document.getElementById("past-races");
+pastRaces.appendChild(newLi);
+
+
+
+
+
+// Part 7
+// Create a new .blog-post corresponding to the new city added in Part 6. You will have to create a new <div> with class of .blog-post, a new <h2> with text, and a new <p> with some text. Think about what order you want to create the elements, and what order you want to append them in.
+
+
+
+// Part 8
+// When you reload the page, the script.js file loads a random DOM quote. Let's play with the included function:
+// Query select the #quote-title ID element and add a click event handler. That event handler should use the function randomQuotewhenever #quote-title is clicked.
+
+
+
+// Part 9
+// Select all .blog-post class elements. Iterate through the list of .blog-post class elements and apply two event handlers to each node. The first event handler should be listening for mouseout events while the second handler should be listening for mouseenter events.
+
+// The mouseouthandler should toggle the class .purple
+// The mouseenterhandler should toggle the class .red
